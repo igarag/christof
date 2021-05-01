873b6b4404dca0720926da5c9f97946e968cadd3
igarag <n.arranz.agueda@gmail.com>
2021-05-01 21:56:42 +0200

    :sparkles: new camera endpoint

---
+++ app/routes/camera.py
@@ -0,0 +1,26 @@
+# import io
+# from starlette.responses import StreamingResponse
+
+from fastapi import APIRouter, Response
+
+from app.conf.settings import logger
+from app.services.security_webcam import gen_frames
+
+router = APIRouter(
+    prefix=""
+)
+
+
+@router.post('/video-feed')
+def video_feed() -> Response:
+    logger.debug(f'Sending frames')
+    headers = {'mimetype': 'multipart/x-mixed-replace; boundary=frame'}
+    return Response(gen_frames(), headers=headers)
+
+
+# @app.post("/vector-image")
+# def image_endpoint(*, vector):
+#     # Returns a cv2 image array from the document vector
+#     cv2img = my_function(vector)
+#     res, im_png = cv2.imencode(".png", cv2img)
+#     return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
