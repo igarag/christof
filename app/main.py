c384437d920e7cdc7e8b77923149ad5f971032ab
igarag <n.arranz.agueda@gmail.com>
2021-05-08 22:55:27 +0200

    :rotating_lights: PEP8 rules

--- app/main.py
+++ app/main.py
@@ -20,8 +20,10 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get('/')
 def index(request: Request):
-    return templates.TemplateResponse('index.html', context={'request': request})
+    return templates.TemplateResponse('index.html',
+                                      context={'request': request})
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
